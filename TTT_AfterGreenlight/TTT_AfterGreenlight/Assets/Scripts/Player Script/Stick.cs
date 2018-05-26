9dc3eaf824703ab6d7c6b371312d53f827c44078
rabu-nee <lylybui98@gmail.com>
2018-05-26 18:52:06 +0200

    stick can stick on wall now

---
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/Player Script/Stick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Wall")
+        {
+            this.GetComponent<Rigidbody2D>().isKinematic = true;
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+}
