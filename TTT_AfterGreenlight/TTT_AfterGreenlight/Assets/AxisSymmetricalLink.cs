c360e016f0d1c936a0c896fd39f9741ac43beae8
Lucah97 <blankflankpictures@gmail.com>
2018-05-28 21:15:24 +0200

    Point Symmetry / Axis Symmetry (+Rotation Angle)

---
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/AxisSymmetricalLink.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisSymmetricalLink : MonoBehaviour {
+
+	public Transform midPoint;
+	public float angleOffset = 0f;
+	public bool x = false;
+	public bool y = true;
+	[HideInInspector]
+	public GameObject copyObj;
+	private GameObject originalObj;
+
+	//BUILT-IN FUNCTIONS===================================================================================================================
+	void Start () {
+		originalObj = transform.GetChild (0).gameObject;
+	}
+
+
+	void FixedUpdate () {
+		//Reset bools
+		if (x && y) {
+			x = false;
+		}
+
+		GameObject.Destroy (copyObj);
+		if (x) {
+			Vector3 newOffset = new Vector3(0, (midPoint.position.y - originalObj.transform.position.y), 0);
+			Vector3 angles = new Vector3 (0, 0, angleOffset);
+			newOffset = Quaternion.Euler (angles) * newOffset;
+			Vector3 newPos = new Vector3(originalObj.transform.position.x, (midPoint.position.y), originalObj.transform.position.z) + newOffset;
+			copyObj = Instantiate (originalObj, newPos, originalObj.transform.rotation, this.gameObject.transform) as GameObject;
+			copyObj.transform.localScale = new Vector3(copyObj.transform.localScale.x, -copyObj.transform.localScale.y, copyObj.transform.localScale.z);
+		}
+		if (y) {
+			Vector3 newOffset = new Vector3((midPoint.position.x - originalObj.transform.position.x), 0, 0);
+			Vector3 angles = new Vector3 (0, 0, angleOffset);
+			newOffset = Quaternion.Euler (angles) * newOffset;
+			Vector3 newPos = new Vector3((midPoint.position.x), originalObj.transform.position.y, originalObj.transform.position.z) + newOffset;
+			copyObj = Instantiate (originalObj, newPos, originalObj.transform.rotation, this.gameObject.transform) as GameObject;
+			copyObj.transform.localScale = new Vector3(-copyObj.transform.localScale.x, copyObj.transform.localScale.y, copyObj.transform.localScale.z);
+		}
+	}
+}
