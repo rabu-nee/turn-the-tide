105eedf5a02c07239e38eeba0d65a816929a231d
Lucah97 <blankflankpictures@gmail.com>
2018-05-28 20:27:56 +0200

    BouncyLine Edit

--- TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/BouncyString.cs
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/BouncyString.cs
@@ -11,21 +11,18 @@ public class BouncyString : MonoBehaviour {
 	private int shiftPosition = -1;
 	private Vector3 standardPosition;
 	public GameObject[] playerObjs = new GameObject[2];
+	private GameObject[] actualPlayers = new GameObject[2];
 	private int playerIndex = 0;
 
 	//BUILT-IN FUNCTIONS===================================================================================================================
 	void Start () {
 		standardPosition = transform.localPosition;
-	}
-
-	void Update () {
-		
+		actualPlayers [0] = GameObject.FindGameObjectWithTag ("Player1");
+		actualPlayers [1] = GameObject.FindGameObjectWithTag ("Player2");
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (isPlayerTag(other.gameObject.tag)) {
 			Debug.Log (other.gameObject.name);
-			//addShift (2 * getPlayerGravity(other.gameObject));
 			if (playerIndex == 0) {
 				playerObjs [playerIndex] = other.gameObject;
 				playerIndex++;
@@ -36,14 +33,12 @@ public class BouncyString : MonoBehaviour {
 				}
 				if (playerIndex == 2) {
 					addVelocities (playerObjs [0], playerObjs [1]);
+					addShift (2 * getPlayerGravity(other.gameObject));
 					playerObjs [0] = playerObjs [1];
 					playerObjs [1] = null;
 					playerIndex--;
 				}
-			}
-
-		}
-			
+			}		
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
@@ -53,22 +48,25 @@ public class BouncyString : MonoBehaviour {
 		}
 		if (playerObjs [1] == other.gameObject) {
 			playerObjs [1] = null;
+			playerIndex--;
 		}
 	}
 
 	//CUSTOM FUNCTIONS===================================================================================================================
 	private void addVelocities(GameObject addObj, GameObject velObj) {
-		Rigidbody2D rb1 = addObj.GetComponent<Rigidbody2D> ();
-		Rigidbody2D rb2 = velObj.GetComponent<Rigidbody2D> ();
-		Vector2 newVel = new Vector2(0, -20 * getPlayerGravity(addObj)) * velocityMultiplier;
-		Debug.Log (newVel.y);
-		rb1.velocity = newVel;
-		rb2.velocity = Vector2.zero;
+		if ((addObj != null) && (velObj != null)) {
+			Rigidbody2D rb1 = addObj.GetComponent<Rigidbody2D> ();
+			Rigidbody2D rb2 = velObj.GetComponent<Rigidbody2D> ();
+			Vector2 newVel = new Vector2 (0, rb2.velocity.y) * velocityMultiplier;
+			Debug.Log (newVel.y);
+			rb1.velocity = newVel;
+			rb2.velocity = Vector2.zero;
+		}
 	}
 
 	private void addShift(int dir) {
 		shiftPosition += dir;
-		shiftPosition = Mathf.Clamp (shiftPosition, -1, 1);
+		shiftPosition = Mathf.Clamp (shiftPosition, -1, 1);	
 
 		//Shift self
 		Vector3 newPos = standardPosition;
@@ -77,10 +75,10 @@ public class BouncyString : MonoBehaviour {
 
 		//Shift Players
 		for (int i = 0; i < 2; i++) {
-			if (playerObjs [i] != null) {
-				newPos = playerObjs [i].transform.localPosition;
-				newPos.y += (shiftAmount * shiftPosition);
-				playerObjs [i].transform.localPosition = newPos;
+			if ((actualPlayers [i] == playerObjs[0]) || (actualPlayers[i] == playerObjs[1])) {
+				newPos = actualPlayers [i].transform.localPosition;
+				newPos.y += ((shiftAmount*3) * shiftPosition);
+				actualPlayers [i].transform.localPosition = newPos;
 			}
 		}
 	}
