4701392b09bd86418d0d964976461eacc9c13923
Lucah97 <blankflankpictures@gmail.com>
2018-05-29 21:20:53 +0200

    adff

---
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/activateDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateDoor : MonoBehaviour {
+
+	public GameObject door;
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag ("Stick")) {
+			if (door != null) {
+				Destroy (door);
+			}
+		}
+	}
+}
