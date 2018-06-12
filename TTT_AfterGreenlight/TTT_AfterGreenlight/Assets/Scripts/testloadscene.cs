4f1a1df505d045ffe27ebf7269784bb6c8c103f7
Unknown <blankflankpictures@gmail.com>
2018-06-12 22:24:44 +0200

    Menu
    Menu Scene

---
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/testloadscene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class testloadscene : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			int nextSceneIndex = SceneManager.GetActiveScene ().buildIndex + 1;
+			SceneManager.LoadScene(nextSceneIndex);
+		}
+	}
+}
