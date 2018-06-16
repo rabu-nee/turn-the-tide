f7316737fd8ae8287edcfa07a02d9e4800d64024
Unknown <blankflankpictures@gmail.com>
2018-06-16 19:14:14 +0200

    Gear Dev, Level 3 Playable

--- TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/GearParent.cs
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/GearParent.cs
@@ -7,8 +7,11 @@ public class GearParent : MonoBehaviour {
 	public float rotationMultiplier = 2f;
 	public float rotationFriction = 1f;
 	public float velocityAmount = 40f;
+	public float startVelocity = 0f;
+	public bool continuous = false;
 
 	private float velocity = 0f;
+	private float desiredVelocity = 0f;
 	private GearScript childGS;
 	private LevelRotation lr;
 	private int lastScreen;
@@ -18,17 +21,31 @@ public class GearParent : MonoBehaviour {
 		childGS = transform.GetChild (0).gameObject.GetComponent<GearScript> ();
 		lr = GameObject.FindGameObjectWithTag ("CurrentLevel").GetComponent<LevelRotation> ();
 		lastScreen = lr.getCurScreen ();
+		velocity = startVelocity;
+		if (!continuous) {
+			desiredVelocity = 0;
+		} else {
+			desiredVelocity = startVelocity * lr.getCurScreen();
+		}
 	}
 
-	void FixedUpdate () {
+	void Update () {
 		//Apply rotation based on Turning
 		if (lr.getCurScreen () != lastScreen) {
 			lastScreen = lr.getCurScreen ();
-			velocity += (velocityAmount * lr.getLastDir ());
+			if (!continuous) {
+				velocity += (velocityAmount * lr.getLastDir ());
+				desiredVelocity = 0;
+			}
+			if (continuous) {
+				desiredVelocity = startVelocity * lr.getLastDir();
+			}
 		}
 
-		//Apply friction to velocity
-		velocity = Mathf.Lerp(velocity, 0f, Time.deltaTime * rotationFriction);
+		//Apply velocities
+		velocity = Mathf.Lerp (velocity, desiredVelocity, Time.deltaTime * rotationFriction);
+		Debug.Log (desiredVelocity);
+
 
 		//Rotate Gears based on velocity
 		childGS.rotate (Time.deltaTime * (velocity * rotationMultiplier));
--- TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/activateDoor.cs
+++ TTT_AfterGreenlight/TTT_AfterGreenlight/Assets/Scripts/activateDoor.cs
@@ -7,11 +7,10 @@ public class activateDoor : MonoBehaviour {
 	public GameObject door;
 
 	void OnCollisionEnter2D(Collision2D other) {
-		//if (other.gameObject.CompareTag ("Stick")) {
+		if ((other.gameObject.CompareTag ("Stick")) || (other.gameObject.CompareTag ("Player1")) || (other.gameObject.CompareTag ("Player2"))) {
 			if (door != null) {
 				Destroy (door);
-				//Destroy (other.gameObject);
 			}
-		//}
+		}
 	}
 }
